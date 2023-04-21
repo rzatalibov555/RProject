23c0d901fae2fa7a54b6f34a2294d63f1c748ac5
rzatalibov555 <sethubpro@gmail.com>
2023-04-21 16:37:34 +0400

    login page create ind integration

--- RPoject/urls.py
+++ RPoject/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("products.urls")),
+    path('', include("accounts.urls")),
 ]
 
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,6 @@
+from django.shortcuts import render
+from .forms import LoginForm
 
+def login_view(request):
+    context = {}
+    return render(request, "accounts/login.html", context)
