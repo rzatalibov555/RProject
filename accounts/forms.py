6f877230815cac24a69991479c86ef870813db24
rzatalibov555 <sethubpro@gmail.com>
2023-04-23 17:48:21 +0400

    login form finish. Username and password, Check user, form errors, login, redirect

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,47 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 User = get_user_model()
 
 class LoginForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput) #Change password input type to password
+
     class Meta:
         model = User
         fields = ("username", "password")
+
+    # ====================== FORM VALIDATIONS Start ======================
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        user = authenticate(username=username, password=password)
+        print(user)
+
+        if not user:
+            raise forms.ValidationError("Username or password is wrong!")
+
+        if not user.is_active:
+            raise forms.ValidationError("Your account is not active!")
+
+        return self.cleaned_data
+
+    # ====================== FORM VALIDATIONS End   ======================
+
+
+    # ====================== for add form-control class to all inputs Start ======================
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class": "form-control"})
+            self.fields[field].required = True
+
+    # ====================== for add form-control class to all inputs End ======================
+
+
+
+
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 def login_view(request):
-    context = {}
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST or None)
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect("/")
+        else:
+            print(form.errors)
+
+    context = {
+        "form":form
+    }
     return render(request, "accounts/login.html", context)
