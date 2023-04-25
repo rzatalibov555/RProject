b37ec8caeff6c5f8167c4c0c5c6c8bf58c64bce0
rzatalibov555 <sethubpro@gmail.com>
2023-04-25 19:11:22 +0400

    add product img to product create menu admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 from .models import Category, Product, ProductImage
 
 admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ProductImage)
+# admin.site.register(Product)
+# admin.site.register(ProductImage)
+
+class ProductImageAdmin(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Product
+
+    inlines = (ProductImageAdmin, )
+
+admin.site.register(Product, ProductAdmin)
