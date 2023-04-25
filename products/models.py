11f88cf9b50051699b7d67a6a4b87c6725a65197
rzatalibov555 <sethubpro@gmail.com>
2023-04-25 16:43:25 +0400

    start project. Products index, create urls, create directory static for css, js ... files, load static for html, install package MPTT for multicategory

---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
---
+++ products/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index_view(request):
+    context = {}
+    return render(request, "products/list.html", context)
